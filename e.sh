13881bcc7874b192f0818d5ea70fb90df8254ab5
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-06-19 10:45:41 -0400

    Delete e.sh

--- e.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git init 
-git config user.name "Amorim-EA"
-git config user.email "erick.amorim@estudante.ifms.edu.br"
-git remote add origin "https://github.com/Amorim-EA/Projeto_Interdisciplinar.git"
-git add .
-git commit -m "Adicionado as pastas back e front, contendo todos os arquivos importantes, fazendo a separação entre o server e o client" 
-git push origin master 
