bca8d433bd36a7002ebc2f6f76ffd0404e594f00
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-06-26 15:44:45 -0400

    Update page.js

--- front/src/app/cadastro/page.js
+++ front/src/app/cadastro/page.js
@@ -26,7 +26,7 @@ export default function Cadastro() {
 
         const produtoJson = JSON.stringify(produto);
 
-        fetch("http://localhost:3003/produtos/", {
+        fetch("https://projeto-interdisciplinar-seven.vercel.app/produtos/", {
             method: "POST",
             headers: { "content-Type": "application/json" },
             body: produtoJson
