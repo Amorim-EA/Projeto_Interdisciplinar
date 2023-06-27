e33e4efb30c6cafb888ffa7b26c68e6dff027e7a
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-06-27 07:12:26 -0400

    Correçãozinha layout.js

--- front/src/app/layout.js
+++ front/src/app/layout.js
@@ -1,8 +1,5 @@
 import style from './page.module.css';
-import { Inter } from 'next/font/google';
 import Link from 'next/link';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
